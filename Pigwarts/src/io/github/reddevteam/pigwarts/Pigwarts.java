283f0ceccc02be4fab4ef40505c401081a7dd67f
zacklukem <mayhew.zachary2003@gmail.com>
2015-08-17 20:23:33 -0700

    Updated plugin

--- Pigwarts/src/io/github/reddevteam/pigwarts/Pigwarts.java
+++ Pigwarts/src/io/github/reddevteam/pigwarts/Pigwarts.java
@@ -1,17 +1,52 @@
 package io.github.reddevteam.pigwarts;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
 public class Pigwarts extends JavaPlugin {
-	
+	private ScoreboardManager sbm = Bukkit.getScoreboardManager();
+	public Scoreboard board = sbm.getNewScoreboard();
+	@SuppressWarnings("unused")
+	private Objective timeOnline;
 	@Override
     public void onEnable() {
-		getLogger().info("Pigwarts: Pigwarts has started initialization.");
+		JavaPlugin plugin = new JavaPlugin() {
+		};
 		// TODO Insert logic to be performed when the plugin is enabled
-		getLogger().info("Pigwarts: Pigwarts has initialized!");
+		File f = new File(plugin.getDataFolder() + "/");
+		if(!f.exists()) {
+			getLogger().info("Creating Plugin Folder");
+		    f.mkdir();
+		    
+		}
+		File fInit = new File(plugin.getDataFolder() + "/fInit.fak");
+	    if (!fInit.exists()) {
+	    	// Plugin's first run initialization
+	    	{
+	    		
+	    		timeOnline = board.registerNewObjective("pigwarts_timeOnline", "stat.playOneMinute");
+	    	}
+	    	
+	    	// Don't mess with this - it makes it so that the first run initialization doesnt run again
+	    	{
+		    	getLogger().info("Creating fInit Check");
+		    	try {
+					fInit.createNewFile();
+				} catch (IOException e) {
+					getLogger().severe("IOException (File error)");
+					e.printStackTrace();
+				}
+	    	}
+	    }
+		getLogger().info("Pigwarts has initialized!");
     }
  
     @Override
@@ -20,10 +55,10 @@ public class Pigwarts extends JavaPlugin {
     	getLogger().info("Pigwarts: goodbye!");
     }
     
+    @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    	if (cmd.getName().equalsIgnoreCase("pigwarts") && sender instanceof Player) {
-    		Player player = (Player) sender;
-    		player.sendMessage("hello, " + player.getName());
+    	if (cmd.getName().equalsIgnoreCase("pigwarts")) {
+    		sender.sendMessage("Running Pigwarts");
     		
     		return true;
     	}
---
+++ Pigwarts/src/io/github/zacklukem/clock/Timer.java
@@ -0,0 +1,10 @@
+package io.github.zacklukem.clock;
+
+import java.util.Date;
+
+public class Timer {
+	public int time;
+	public void initializeTimer() {
+		
+	}
+}
